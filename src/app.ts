606809b01634704a249fd6b35f789726e8d88c73
Leonardo Xavier <leonardoxw@gmail.com>
2026-07-26 17:15:16 -0300

    feat: implement customer listing

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { listCustomers } from "./customers/customer.repository";
 
 export const app = express();
 
@@ -9,10 +10,15 @@ app.get("/health", (_request, response) => {
 });
 
 app.get("/customers", (_request, response) => {
-  response.status(501).json({
-    error: "Not implemented",
-    task: "API-001"
-  });
+
+  const customers = listCustomers();
+
+  const result = {
+    data: customers,
+    total: customers.length
+  }
+
+  response.status(200).json(result);
 });
 
 app.use((_request, response) => {
